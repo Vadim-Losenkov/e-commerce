308c8c973331ce769e478cad5a38c6ceaa4853cd
vadim <vadimlosenkov0729@gmail.com>
2021-03-27 10:31:27 +0300

    fixed some bugs in catalog page

--- app/js/main.js
+++ app/js/main.js
@@ -10,10 +10,17 @@ $(function() {
   $headerMenu.on('click', () => $('.header__nav-list').slideToggle(300))
   $headerMenu.on('click', () => $('.header__nav-menu--arrow').toggleClass('open'))
   
-  $('.filter__item-link').on('click', function () {
+  $('.filter__item-link').on('click', function (event) {
+    event.preventDefault()
     $('.filter__item-link.active').removeClass('active')
     $(this).toggleClass('active')
   })
+
+  const $itemLike = $('.catalog__item-like')
+  $itemLike.on('click', function() {
+    $(this).toggleClass('active')
+  })
+
 })
 
 const tabs = new Tabs('[data-menu="menu"]', '[data-menu="items"]')
