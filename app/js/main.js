34d200fd86f25830b2e6a78798f61968b1b9e121
Vadim Losenkov <vprog0729@gmail.com>
2021-03-27 08:53:33 +0300

    added catalog page

--- app/js/main.js
+++ app/js/main.js
@@ -9,4 +9,11 @@ $(function() {
   const $headerMenu = $('.header__nav-menu')
   $headerMenu.on('click', () => $('.header__nav-list').slideToggle(300))
   $headerMenu.on('click', () => $('.header__nav-menu--arrow').toggleClass('open'))
+  
+  $('.filter__item-link').on('click', function () {
+    $('.filter__item-link.active').removeClass('active')
+    $(this).toggleClass('active')
+  })
 })
+
+const tabs = new Tabs('[data-menu="menu"]', '[data-menu="items"]')
